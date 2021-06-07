f650bfb878794a055b69a4067a7d64b1750b1fae
masu1234 <yuto.masunaka.s6@alumni.tohoku.ac.jp>
2021-06-07 13:18:10 +0900

    first commit

---
+++ simple_crawler2/lib/simple_crawler2.ex
@@ -0,0 +1,18 @@
+defmodule SimpleCrawler2 do
+  @moduledoc """
+  Documentation for `SimpleCrawler2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SimpleCrawler2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
